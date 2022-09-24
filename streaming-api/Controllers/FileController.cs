3d8c32193c2dcf3b95e677e8d786ac4d7f4b74c3
Hakan Hidir <hakan.hidir@technicsatelier.com>
2022-09-24 04:34:33 +0200

    stremaing init on api

---
+++ streaming-api/Controllers/FileController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace streaming_api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FileController : ControllerBase
+{
+    private readonly ILogger<FileController> _logger;
+
+    public FileController(ILogger<FileController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public async IAsyncEnumerable<string> GetFileStream()
+    {
+        string dxfLine;
+        var streamReader = new StreamReader(AppContext.BaseDirectory + "zero_to_one.dxf");
+        while ((dxfLine = await streamReader.ReadLineAsync()) != null)
+        {
+            yield return dxfLine;
+        }
+    }
+
+    //Consume
+    //var lines = GetFileStream()
+    //await foreach(var line in lines)
+    //{
+
+    //}
+}
+
